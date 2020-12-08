236d8a64d03861a3a6ac17ae20bcd64d49bba1d5
RudraPramanik <43309139+RudraPramanik@users.noreply.github.com>
2020-12-08 17:31:00 +0600

    Add files via upload

---
+++ build/precache-manifest.58be14a8eaf12f490484661cee3750e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb77eebca1d2d1aa15f6255e4dd365eb",
+    "url": "/RudraPramanik/taskManeger/index.html"
+  },
+  {
+    "revision": "9efd836c5549bb964df5",
+    "url": "/RudraPramanik/taskManeger/static/css/main.2b9ab736.chunk.css"
+  },
+  {
+    "revision": "80ccdfe89238c4d39f99",
+    "url": "/RudraPramanik/taskManeger/static/js/2.b566954d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RudraPramanik/taskManeger/static/js/2.b566954d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9efd836c5549bb964df5",
+    "url": "/RudraPramanik/taskManeger/static/js/main.9913363f.chunk.js"
+  },
+  {
+    "revision": "98232b6def5f9ed1867b",
+    "url": "/RudraPramanik/taskManeger/static/js/runtime-main.543fa4a6.js"
+  }
+]);
---
+++ build/service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/RudraPramanik/taskManeger/precache-manifest.58be14a8eaf12f490484661cee3750e4.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/RudraPramanik/taskManeger/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
